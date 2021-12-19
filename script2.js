785a1f57ce94e915f48693a1f342cefa8e62d981
Oh Dylan <ddragon1128@hotmail.com>
2021-12-19 22:02:01 +0800

    Initialise the commit

---
+++ script2.js
@@ -0,0 +1,9 @@
+const findNames = (term, db) => {
+    const matches = db.filter(names => {
+        return names.includes(term);
+    });
+    // We only want the first three of search results.
+    return matches.length > 3 ? matches.slice(0, 3) : matches;
+}
+
+module.exports = findNames;
---
+++ script3.js
@@ -0,0 +1,16 @@
+const fetch = require('isomorphic-fetch');
+
+const fetchPokemon = async (pokemon, fetch) => {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    const results = await fetch(apiUrl);
+    const data = await results.json();
+    return {
+        name: data.name,
+        height: data.height,
+        weight: data.weight
+    };
+};
+
+fetchPokemon("bulbasaur", fetch);
+
+module.exports = fetchPokemon;
